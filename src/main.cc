5209567907e3caf0537c77734eb3ee2b3196115f
amc-nu <abrahammonrroy@yahoo.com>
2020-11-19 12:40:13 +0900

    code formatting
    Signed-off-by: amc-nu <abrahammonrroy@yahoo.com>

--- src/main.cc
+++ src/main.cc
@@ -6,15 +6,19 @@
 #include <pcl/point_types.h>
 #include "pandarGeneral_sdk/pandarGeneral_sdk.h"
 
-ros::Time resolveHourAmbiguity(const ros::Time &stamp, const ros::Time &nominal_stamp) {
+ros::Time resolveHourAmbiguity(const ros::Time &stamp, const ros::Time &nominal_stamp)
+{
   const int HALFHOUR_TO_SEC = 1800;
   ros::Time retval = stamp;
-  if (nominal_stamp.sec > stamp.sec) {
-    if (nominal_stamp.sec - stamp.sec > HALFHOUR_TO_SEC) {
-      retval.sec = retval.sec + 2*HALFHOUR_TO_SEC;
+  if (nominal_stamp.sec > stamp.sec)
+  {
+    if (nominal_stamp.sec - stamp.sec > HALFHOUR_TO_SEC)
+    {
+      retval.sec = retval.sec + 2 * HALFHOUR_TO_SEC;
     }
-  } else if (stamp.sec - nominal_stamp.sec > HALFHOUR_TO_SEC) {
-    retval.sec = retval.sec - 2*HALFHOUR_TO_SEC;
+  } else if (stamp.sec - nominal_stamp.sec > HALFHOUR_TO_SEC)
+  {
+    retval.sec = retval.sec - 2 * HALFHOUR_TO_SEC;
   }
   return retval;
 }
@@ -24,7 +28,7 @@ class HesaiLidarClient
 private:
   ros::Publisher lidar_publisher_;
   ros::Publisher packet_publisher_;
-  PandarGeneralSDK* pandar_sdk_ptr_;
+  PandarGeneralSDK *pandar_sdk_ptr_;
   std::string publish_type_;
   std::string timestamp_type_;
   ros::Subscriber packet_subscriber_;
@@ -58,10 +62,12 @@ public:
     private_handle.getParam("timestamp_type", timestamp_type_);
     private_handle.param<bool>("use_rosbag", use_rosbag_, false);
 
-    if(!pcap_file.empty()){
+    if (!pcap_file.empty())
+    {
       pandar_sdk_ptr_ = new PandarGeneralSDK(pcap_file, boost::bind(&HesaiLidarClient::lidarCallback, this, _1, _2, _3), \
       static_cast<int>(start_angle * 100 + 0.5), 0, pcl_data_type, lidar_type, timestamp_type_);
-      if (pandar_sdk_ptr_ != NULL) {
+      if (pandar_sdk_ptr_ != NULL)
+      {
         std::ifstream fin(lidar_correction_file);
         int length = 0;
         std::string strlidarCalibration;
@@ -75,13 +81,16 @@ public:
         pandar_sdk_ptr_->LoadLidarCorrectionFile(strlidarCalibration);
       }
     }
-    if (use_rosbag_){
+    if (use_rosbag_)
+    {
       ROS_INFO_STREAM("Rosbag");
-      packet_subscriber_ = global_nh.subscribe("pandar_packets",10,&HesaiLidarClient::scanCallback, (HesaiLidarClient*)this, ros::TransportHints().tcpNoDelay(true));
+      packet_subscriber_ = global_nh.subscribe("pandar_packets", 10, &HesaiLidarClient::scanCallback,
+                                               (HesaiLidarClient *) this, ros::TransportHints().tcpNoDelay(true));
 
       pandar_sdk_ptr_ = new PandarGeneralSDK("", boost::bind(&HesaiLidarClient::lidarCallback, this, _1, _2, _3), \
       static_cast<int>(start_angle * 100 + 0.5), 0, pcl_data_type, lidar_type, timestamp_type_);
-      if (pandar_sdk_ptr_ != NULL) {
+      if (pandar_sdk_ptr_ != NULL)
+      {
         std::ifstream fin(lidar_correction_file);
         int length = 0;
         std::string strlidarCalibration;
@@ -94,18 +103,20 @@ public:
         strlidarCalibration = buffer;
         pandar_sdk_ptr_->LoadLidarCorrectionFile(strlidarCalibration);
       }
-    }
-    else {
-      packet_publisher_ = global_nh.advertise<hesai_lidar::PandarScan>("pandar_packets",10);
+    } else
+    {
+      packet_publisher_ = global_nh.advertise<hesai_lidar::PandarScan>("pandar_packets", 10);
       pandar_sdk_ptr_ = new PandarGeneralSDK(lidar_ip, lidar_recv_port, gps_port, \
         boost::bind(&HesaiLidarClient::lidarCallback, this, _1, _2, _3), \
         NULL, static_cast<int>(start_angle * 100 + 0.5), 0, pcl_data_type, lidar_type, timestamp_type_);
     }
-    
-    if (pandar_sdk_ptr_ != NULL) {
-        pandar_sdk_ptr_->Start();
-    } else {
-        ROS_ERROR_STREAM(ros::this_node::getName() << "Error while creating Hesai Lidar Object");
+
+    if (pandar_sdk_ptr_ != NULL)
+    {
+      pandar_sdk_ptr_->Start();
+    } else
+    {
+      ROS_ERROR_STREAM(ros::this_node::getName() << "Error while creating Hesai Lidar Object");
     }
   }
 
@@ -117,23 +128,22 @@ public:
     ros::Time sensor_time = ros::Time(timestamp);
     ros::Time sensor_time_ok = resolveHourAmbiguity(sensor_time, system_time);
 
-    if (abs(int(sensor_time_ok.toSec())-int(system_time.toSec())) < 1)
+    if (abs(int(sensor_time_ok.toSec()) - int(system_time.toSec())) < 1)
     {
       pcl_conversions::toPCL(ros::Time(sensor_time_ok), cld->header.stamp);
-    }
-    else
+    } else
     {
       pcl_conversions::toPCL(ros::Time(system_time), cld->header.stamp);
       ROS_WARN_STREAM_THROTTLE(10, ros::this_node::getName()
-      << " | Using system time. Large Time difference between device and system. Sensor:"
-      << sensor_time_ok << " System:" << system_time << ". Check clock source (GPS/PTP)");
+        << " | Using system time. Large Time difference between device and system. Sensor:"
+        << sensor_time_ok << " System:" << system_time << ". Check clock source (GPS/PTP)");
     }
 
     sensor_msgs::PointCloud2 output;
     pcl::toROSMsg(*cld, output);
     lidar_publisher_.publish(output);
 
-    if(!use_rosbag_)
+    if (!use_rosbag_)
     {
       packet_publisher_.publish(scan);
     }
